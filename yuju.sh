204751642ec23e025173616724a858cbdc5c527b
yuju <39904736+yuju520@users.noreply.github.com>
2024-10-29 11:36:12 +0800

    跳过starship确认流程

--- yuju.sh
+++ yuju.sh
@@ -813,7 +813,7 @@ download_zsh(){
     clear
     apt install zsh -y && apt install curl -y
 	chsh -s $(which zsh)
-    curl -sS https://starship.rs/install.sh | sh
+    curl -sS https://starship.rs/install.sh | sh -s -- --yes
     echo 'eval "$(starship init zsh)"' >> ~/.zshrc
     echo "zsh+Starship安装完成,重新登录终端即可启用"
 }
