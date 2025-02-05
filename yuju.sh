93a5ccb119d2094f520e29ab363526fd64be1806
yuju <39904736+yuju520@users.noreply.github.com>
2025-02-05 13:41:26 +0800

    更新国内安装docker的命令

--- yuju.sh
+++ yuju.sh
@@ -789,8 +789,7 @@ docker_install(){
     # 判断国家是否为中国（CN）
     if [ "$country" == "CN" ]; then
         echo "本机器地理位置为中国，正在使用国内安装脚本..."
-        curl https://install.1panel.live/docker-install -o docker-install
-        sudo bash ./docker-install
+	sudo curl -fsSL https://get.docker.com| bash -s docker --mirror Aliyun
         rm -f ./docker-install
         echo "Docker安装完成，正在切换镜像源（由1panel提供）..."
         touch /etc/docker/daemon.json
